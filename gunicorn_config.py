1ac3bfc9adaa36760365f48ca3a2670a9cb099c3
Ken Lee <keun.lee@thekeunster.com>
2021-09-23 01:58:56 -0500

    update values

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,9 +1,9 @@
 import os
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
-threads = int(os.environ.get('GUNICORN_THREADS', '1'))
-timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
+workers = 4 # int(os.environ.get('GUNICORN_PROCESSES', '3'))
+threads = 1 # int(os.environ.get('GUNICORN_THREADS', '1'))
+timeout = 120 # int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+bind = '0.0.0.0:8080' #os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }

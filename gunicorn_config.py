341eb4ae9d01fa443ad0494075bf14f63dcc8dc5
Ken Lee <keun.lee@thekeunster.com>
2021-09-23 02:12:29 -0500

    update values

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,6 @@
 import os
 
-workers = 4 # int(os.environ.get('GUNICORN_PROCESSES', '3'))
+workers = 6 # int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = 1 # int(os.environ.get('GUNICORN_THREADS', '1'))
 timeout = 120 # int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 bind = '0.0.0.0:8080' #os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
